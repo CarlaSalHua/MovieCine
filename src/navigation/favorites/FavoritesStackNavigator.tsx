85ea18f1c2b3440086d525a160bf2e14c19e28e5
CarlaSalHua <c.salhua97@gmail.com>
2025-10-04 23:52:14 -0500

    Saved Stack

---
+++ src/navigation/favorites/FavoritesStackNavigator.tsx
@@ -0,0 +1,31 @@
+import FavoriteScreen from '@/screens/FavoriteScreen/FavoriteScreen';
+import MovieDetailScreen from '@/screens/MovieDetailScreen/MovieDetailScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
+
+const FavoritesStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle: { backgroundColor: '#171010' },
+        headerStyle: { backgroundColor: '#171010' },
+        headerTintColor: 'white',
+      }}
+    >
+      <Stack.Screen
+        name="SavedHome"
+        component={FavoriteScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        component={MovieDetailScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default FavoritesStackNavigator;
