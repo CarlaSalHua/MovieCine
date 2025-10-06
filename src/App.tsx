d67578b6ded237977f3971c1716703e3ef662623
CarlaSalHua <c.salhua97@gmail.com>
2025-10-06 00:08:06 -0500

    updateApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import {
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+import React, { useEffect } from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import { store } from '@/store';
-import AppNavigator from './navigation/AppNavigator';
 
+import AppNavigator from './navigation/AppNavigator';
+import { store, useAppDispatch } from '@/store';
+import { loadSaved } from '@/features/saved/savedSlice';
+
+const AppContent = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadSaved());
+  }, [dispatch]);
+
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppNavigator />
+    </>
+  );
+};
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AppNavigator />
+        <AppContent />
       </SafeAreaProvider>
     </Provider>
   );
