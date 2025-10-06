3587454fb1d3c83af7be8062dd0def7a4380b678
CarlaSalHua <c.salhua97@gmail.com>
2025-10-06 00:15:09 -0500

    store / middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch, useSelector } from 'react-redux'
-import moviesReducer from '@/features/movies/moviesSlice'
-import savedReducer from '@/features/saved/savedSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import moviesReducer from '@/features/movies/moviesSlice';
+import savedReducer from '@/features/saved/savedSlice';
 
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
-    saved: savedReducer
+    saved: savedReducer,
   },
-})
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['saved/load', 'saved/persist'],
+        ignoredPaths: ['saved.items'],
+      },
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
